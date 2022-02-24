62ab7ec16050e546d636cd3c9ae825fa389793f7
Theo Tyburn <theo.tyburn@plasma.city>
2022-02-24 09:20:20 +0100

    Add blender-inferior

--- packages.scm
+++ packages.scm
@@ -1,161 +1,175 @@
-;; This "manifest" file can be passed to 'guix package -m' to reproduce
-;; the content of your profile.  This is "symbolic": it only specifies
-;; package names.  To reproduce the exact same profile, you also need to
-;; capture the channels being used, as returned by "guix describe".
-;; See the "Replicating Guix" section in the manual.
+(use-modules (guix inferior)
+			 (guix channels)
+             (srfi srfi-1))   ;for 'first'
 
-(specifications->manifest
- (list
-  ;; disk
-  ;; "parted"
+(define inferior-blender-2.90
+  (inferior-for-channels
+   (list (channel
+          (name 'guix)
+          (url "https://git.savannah.gnu.org/git/guix.git")
+          (branch "master")
+          (commit "48d38125c20daa7d89f25245e4463df41e6c957f")
+          (introduction
+           (make-channel-introduction
+            "9edb3f66fd807b096b48283debdcddccfea34bad"
+            (openpgp-fingerprint
+             "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))))
 
-  ;; scripting
-  "perl" ;; TODO: add perl as propagated input for fzf
+(packages->manifest
+ (cons (first (lookup-inferior-packages inferior-blender-2.90 "blender"))
+	   (map specification->package
+			(list
+			 ;; disk
+			 ;; "parted"
 
-  ;; building
-  ;; "make"
+			 ;; scripting
+			 "perl" ;; TODO: add perl as propagated input for fzf
 
-  ;; files
-  "zip" "unzip" "p7zip"
-  "file"
+			 ;; building
+			 ;; "make"
 
-  ;; shell
-  "bash" "zsh"
-  "fzf"
-  "fd"
-  "kitty" "gnome-terminal" ;; TODO: add kitty-themes
-  "xsel"
-  "the-silver-searcher" ;; ag
+			 ;; files
+			 "zip" "unzip" "p7zip"
+			 "file"
 
-  ;; security
-  "openssh"
-  "gnupg"
-  "pinentry"
+			 ;; shell
+			 "bash" "zsh"
+			 "fzf"
+			 "fd"
+			 "kitty" "gnome-terminal" ;; TODO: add kitty-themes
+			 "xsel"
+			 "the-silver-searcher" ;; ag
 
-  ;; fonts
-  "gs-fonts" "font-dejavu" "font-gnu-freefont"
-  "font-fantasque-sans"
-  "font-adobe-source-han-sans" ;;asian fonts
+			 ;; security
+			 "openssh"
+			 "gnupg"
+			 "pinentry"
 
-  ;; desktop setup
-  "redshift"
-  "rclone"
-  "xclip"
-  "maim"
-  "xrandr"
-  "numlockx"
-  "xdg-utils"
-  
-  ;; theme
-  "xsettingsd"
-  "lxappearance" ;; gtk
-  "qt5ct" ;; qt
-  "materia-theme" ;; theme
-  "breeze-icons" ;; icons
-  "mate-themes" ;; cursor
+			 ;; fonts
+			 "gs-fonts" "font-dejavu" "font-gnu-freefont"
+			 "font-fantasque-sans"
+			 "font-adobe-source-han-sans" ;;asian fonts
 
-  ;; system monitoring
-  "duc" "kdf"
-  "htop"
-  "glances"
-  "lsof"
+			 ;; desktop setup
+			 "redshift"
+			 "rclone"
+			 "xclip"
+			 "maim"
+			 "xrandr"
+			 "numlockx"
+			 "xdg-utils"
+			 
+			 ;; theme
+			 "xsettingsd"
+			 "lxappearance" ;; gtk
+			 "qt5ct" ;; qt
+			 "materia-theme" ;; theme
+			 "breeze-icons" ;; icons
+			 "mate-themes" ;; cursor
 
-  ;; package management
-  "flatpak"
-  "nix"
-  
-  ;; documents
-  "xpdf"
-  "evince"
-  "libreoffice"
-  
-  ;; communication
-  "telegram-desktop"
-  "mu"
-  "offlineimap3"
-  
-  ;; web
-  "node"
-  "icecat" "ungoogled-chromium" "firefox"
-  "transmission"
-  ;; "filezilla"
-  "curl"
+			 ;; system monitoring
+			 "duc" "kdf"
+			 "htop"
+			 "glances"
+			 "lsof"
 
-  ;; spelling
-  "hunspell"
-  "hunspell-dict-de"
-  "hunspell-dict-fr"
-  "hunspell-dict-en"
-  
-  ;; ;; latex
-  ;; "texlive-base"
-  ;; ;; math
-  ;; "texlive-txfonts"
-  ;; "texlive-amsfonts"
-  ;; "texlive-latex-amscls"
-  ;; ;; "texlive-latex-mathtools"
-  ;; ;; org-mode export
-  ;; "texlive-cm"
-  ;; "texlive-latex-graphics"
-  ;; "texlive-latex-oberdiek"
-  ;; "texlive-latex-wrapfig"
-  ;; "texlive-generic-ulem"
-  ;; "texlive-latex-capt-of"
-  ;; "texlive-latex-hyperref"
-  ;; "texlive-cm-super"
-  ;; "texlive-fonts-ec"
+			 ;; package management
+			 ;; "flatpak"
+			 ;; "nix"
+			 
+			 ;; documents
+			 "xpdf"
+			 "evince"
+			 "libreoffice"
+			 
+			 ;; communication
+			 "telegram-desktop"
+			 "mu"
+			 "offlineimap3"
+			 
+			 ;; web
+			 ;; "node"
+			 ;; "icecat"
+			 "ungoogled-chromium"
+			 "firefox"
+			 "transmission"
+			 ;; "filezilla"
+			 "curl"
 
-  ;; version control
-    "git"
-  ;; "git-cal"
-  ;; "subversion"
-  
-  ;; data
-  ;; "sequeler"
-  ;; "sqlite"
-  
-  ;; docker
-  "docker"
-  "docker-cli"
-  "containerd"
-  "docker-compose"
-  
-  ;; video
-  "vlc" "mplayer"
-  "obs"
-  "ffmpeg"
+			 ;; spelling
+			 "hunspell"
+			 "hunspell-dict-de"
+			 "hunspell-dict-fr"
+			 "hunspell-dict-en"
+			 
+			 ;; ;; latex
+			 ;; "texlive-base"
+			 ;; ;; math
+			 ;; "texlive-txfonts"
+			 ;; "texlive-amsfonts"
+			 ;; "texlive-latex-amscls"
+			 ;; ;; "texlive-latex-mathtools"
+			 ;; ;; org-mode export
+			 ;; "texlive-cm"
+			 ;; "texlive-latex-graphics"
+			 ;; "texlive-latex-oberdiek"
+			 ;; "texlive-latex-wrapfig"
+			 ;; "texlive-generic-ulem"
+			 ;; "texlive-latex-capt-of"
+			 ;; "texlive-latex-hyperref"
+			 ;; "texlive-cm-super"
+			 ;; "texlive-fonts-ec"
 
-  ;; image
-  "gimp"
-  "imagemagick"
-  "imv"
-  ;; "gphoto2" ; mtp, ptp
-  
-  ;; audio
-  "pavucontrol"
-  "pulseaudio"
-  "blueman"
-  "alsa-utils"
-  ;; "supercollider"
-  ;; "emacs-scel"
-  ;; "jack"
-  ;; "qjackctl"
-    
-  ;; 3D
-  "blender"
-  ;; "gpick"
-  "glslviewer@1.7.0"
+			 ;; version control
+			 "git"
+			 ;; "git-cal"
+			 ;; "subversion"
+			 
+			 ;; data
+			 ;; "sequeler"
+			 ;; "sqlite"
+			 
+			 ;; docker
+			 "docker"
+			 "docker-cli"
+			 "containerd"
+			 "docker-compose"
+			 
+			 ;; video
+			 "vlc" "mplayer"
+			 "obs"
+			 "ffmpeg"
 
-  ;; emacs
-  "emacs-xwidgets-native-comp"
-  "emacs-xwidgets-exwm-native-comp"
-  "emacs-pdf-tools"
+			 ;; image
+			 "gimp"
+			 "imagemagick"
+			 "imv"
+			 ;; "gphoto2" ; mtp, ptp
+			 
+			 ;; audio
+			 "pavucontrol"
+			 "pulseaudio"
+			 "blueman"
+			 "alsa-utils"
+			 ;; "supercollider"
+			 ;; "emacs-scel"
+			 ;; "jack"
+			 ;; "qjackctl"
+			 
+			 ;; 3D
+			 ;; "gpick"
+			 "glslviewer@1.7.0"
 
-  ;; android
-  ;; "adb"
-  ;; "kdeconnect"
+			 ;; emacs
+			 "emacs-xwidgets-native-comp"
+			 "emacs-xwidgets-exwm-native-comp"
+			 "emacs-pdf-tools"
 
-  ;;games
-  "vitetris"
-  ;; "steam"
-  ))
+			 ;; android
+			 ;; "adb"
+			 ;; "kdeconnect"
+
+			 ;;games
+			 "vitetris"
+			 ;; "steam"
+			 ))))
