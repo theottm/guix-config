5f258185aed7b601f27e944457382cfb533baf9c
Theo Tyburn <theo.tyburn@plasma.city>
2022-03-25 23:31:12 +0100

    Update channels

--- channels.scm
+++ channels.scm
@@ -1,52 +1,67 @@
 (list (channel
-        (name 'guix-android)
-        (url "https://framagit.org/tyreunom/guix-android.git")
+        (name 'rde)
+        (url "https://git.sr.ht/~abcdw/rde")
         (branch "master")
-        (commit "c6dbde9bde241c0f535f0fdd2629bd9960ec317d")
+        (commit
+          "1a3044e54795f4c869752c1fd55e1fd8cef9086c")
         (introduction
           (make-channel-introduction
-            "d031d039b1e5473b030fa0f272f693b469d0ac0e"
+            "257cebd587b66e4d865b3537a9a88cccd7107c95"
             (openpgp-fingerprint
-              "1EFB 0909 1F17 D28C CBF9  B13A 53D4 57B2 D636 EE82"))))
+              "2841 9AC6 5038 7440 C7E9  2FFA 2208 D209 58C1 DEB0"))))
       (channel
-        (name 'teddd)
-        (url "/home/teddd/guix/guix-channel")
+        (name 'guixrus)
+        (url "https://git.sr.ht/~whereiseveryone/guixrus")
         (branch "master")
-        (commit "88422b5a0e4bad3c3ca88770b6f77195e68c9f7d")
-		)
-      (channel
-        (name 'flat)
-        (url "https://github.com/flatwhatson/guix-channel.git")
-        (branch "master")
-        (commit "ece373bcd1e00ef746fb7b104e56ab610cc4af2a")
-		)
-      (channel
-        (name 'nonguix)
-        (url "https://gitlab.com/nonguix/nonguix")
-        (branch "master")
-        (commit "9e34a99fe728b374c6b9646b4e6dfd41b145e26b")
+        (commit
+          "5b52f6fb3e436556e951ac4991986d39df9f1958")
         (introduction
           (make-channel-introduction
-            "35533c763a81365e14d0b58ad262e8f88c82872c"
+            "7c67c3a9f299517bfc4ce8235628657898dd26b2"
             (openpgp-fingerprint
-              "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
+              "CD2D 5EAA A98C CB37 DA91  D6B0 5F58 1664 7F8B E551"))))
       (channel
         (name 'guix)
         (url "https://git.savannah.gnu.org/git/guix.git")
         (branch "master")
-        (commit "62fd3cf5b76cd4c5ce460704bcdd549c1f2077dd")
+        (commit
+          "c51961b61f0142e8815be2c4137f907b6d889c26")
         (introduction
           (make-channel-introduction
             "9edb3f66fd807b096b48283debdcddccfea34bad"
             (openpgp-fingerprint
               "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
       (channel
-        (name 'guixrus)
-        (url "https://git.sr.ht/~whereiseveryone/guixrus")
+        (name 'nonguix)
+        (url "https://gitlab.com/nonguix/nonguix")
         (branch "master")
-        (commit "fbe4127fed801b85d58d571526f29d40ac8deb2a")
+        (commit
+          "81a651d3aa006e563385d7ff97f5ba5e5666f142")
         (introduction
           (make-channel-introduction
-            "7c67c3a9f299517bfc4ce8235628657898dd26b2"
+            "35533c763a81365e14d0b58ad262e8f88c82872c"
             (openpgp-fingerprint
-              "CD2D 5EAA A98C CB37 DA91  D6B0 5F58 1664 7F8B E551")))))
+              "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
+      (channel
+        (name 'teddd)
+        (url "/home/teddd/guix/guix-channel")
+        (branch "master")
+        (commit
+          "88422b5a0e4bad3c3ca88770b6f77195e68c9f7d"))
+      (channel
+        (name 'flat)
+        (url "https://github.com/flatwhatson/guix-channel.git")
+        (branch "master")
+        (commit
+          "81bd9f00e7e4677a4deaeb05415c901528521ba9"))
+      (channel
+        (name 'guix-android)
+        (url "https://framagit.org/tyreunom/guix-android.git")
+        (branch "master")
+        (commit
+          "c6dbde9bde241c0f535f0fdd2629bd9960ec317d")
+        (introduction
+          (make-channel-introduction
+            "d031d039b1e5473b030fa0f272f693b469d0ac0e"
+            (openpgp-fingerprint
+              "1EFB 0909 1F17 D28C CBF9  B13A 53D4 57B2 D636 EE82")))))
