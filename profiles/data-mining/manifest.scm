03d4d81a912b9244edeade3e211f25af70e2f708
Theo Tyburn <theo.tyburn@plasma.city>
2022-05-21 13:33:01 +0200

    (data-mining): initialize profile

---
+++ profiles/data-mining/manifest.scm
@@ -0,0 +1,17 @@
+(specifications->manifest
+ (list
+  ;; cli
+  "sparql-query"
+
+  ;; jupyter
+  "jupyter"
+  "graphviz"
+  "python-sparqlkernel"
+
+  ;; emacs
+  "emacs-sparql-mode"
+
+  ;; guile + jupyter
+  ;; "guile-sparql" ;; fails
+  "jupyter-guile-kernel"
+  ))
