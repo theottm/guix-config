e70fc53e92c49d1c9dea92457ba5c5e905412dc3
Theo Tyburn <theo.tyburn@plasma.city>
2022-06-24 12:50:29 +0200

    Update channels

--- channels.scm
+++ channels.scm
@@ -1,48 +1,14 @@
 (list (channel
-        (name 'guix-android)
-        (url "https://framagit.org/tyreunom/guix-android.git")
+        (name 'rde)
+        (url "https://git.sr.ht/~abcdw/rde")
         (branch "master")
         (commit
-          "242b6b7d928a6d4341c2db23a346835732ee3804")
+          "8e2d571380f9c2b3878109a8be77d4b60eda216d")
         (introduction
           (make-channel-introduction
-            "d031d039b1e5473b030fa0f272f693b469d0ac0e"
+            "257cebd587b66e4d865b3537a9a88cccd7107c95"
             (openpgp-fingerprint
-              "1EFB 0909 1F17 D28C CBF9  B13A 53D4 57B2 D636 EE82"))))
-      (channel
-        (name 'teddd)
-        (url "/home/teddd/guix/guix-channel")
-        (branch "master")
-        (commit
-          "4bd390d91d90e0cbc5fc8618f89e2500d90140c0"))
-      (channel
-        (name 'flat)
-        (url "https://github.com/flatwhatson/guix-channel.git")
-        (branch "master")
-        (commit
-          "657da22f0229b978b7bf4e4d476f59f17f6a175f"))
-      (channel
-        (name 'nonguix)
-        (url "https://gitlab.com/nonguix/nonguix")
-        (branch "master")
-        (commit
-          "bffd70d2b4c81db5148deb6d362b13e5dcbbe150")
-        (introduction
-          (make-channel-introduction
-            "35533c763a81365e14d0b58ad262e8f88c82872c"
-            (openpgp-fingerprint
-              "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
-      (channel
-        (name 'guix)
-        (url "https://git.savannah.gnu.org/git/guix.git")
-        (branch "master")
-        (commit
-          "965d54e344dcd19adf3c32f8b9d2dcab62b91e6a")
-        (introduction
-          (make-channel-introduction
-            "9edb3f66fd807b096b48283debdcddccfea34bad"
-            (openpgp-fingerprint
-              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
+              "2841 9AC6 5038 7440 C7E9  2FFA 2208 D209 58C1 DEB0"))))
       (channel
         (name 'guixrus)
         (url "https://git.sr.ht/~whereiseveryone/guixrus")
@@ -55,13 +21,47 @@
             (openpgp-fingerprint
               "CD2D 5EAA A98C CB37 DA91  D6B0 5F58 1664 7F8B E551"))))
       (channel
-        (name 'rde)
-        (url "https://git.sr.ht/~abcdw/rde")
+        (name 'guix)
+        (url "https://git.savannah.gnu.org/git/guix.git")
         (branch "master")
         (commit
-          "8e2d571380f9c2b3878109a8be77d4b60eda216d")
+          "965d54e344dcd19adf3c32f8b9d2dcab62b91e6a")
         (introduction
           (make-channel-introduction
-            "257cebd587b66e4d865b3537a9a88cccd7107c95"
+            "9edb3f66fd807b096b48283debdcddccfea34bad"
             (openpgp-fingerprint
-              "2841 9AC6 5038 7440 C7E9  2FFA 2208 D209 58C1 DEB0")))))
+              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
+      (channel
+        (name 'nonguix)
+        (url "https://gitlab.com/nonguix/nonguix")
+        (branch "master")
+        (commit
+          "bffd70d2b4c81db5148deb6d362b13e5dcbbe150")
+        (introduction
+          (make-channel-introduction
+            "35533c763a81365e14d0b58ad262e8f88c82872c"
+            (openpgp-fingerprint
+              "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
+      (channel
+        (name 'flat)
+        (url "https://github.com/flatwhatson/guix-channel.git")
+        (branch "master")
+        (commit
+          "657da22f0229b978b7bf4e4d476f59f17f6a175f"))
+      (channel
+        (name 'teddd)
+        (url "/home/teddd/guix/guix-channel")
+        (branch "master")
+        (commit
+          "4bd390d91d90e0cbc5fc8618f89e2500d90140c0"))
+      (channel
+        (name 'guix-android)
+        (url "https://framagit.org/tyreunom/guix-android.git")
+        (branch "master")
+        (commit
+          "242b6b7d928a6d4341c2db23a346835732ee3804")
+        (introduction
+          (make-channel-introduction
+            "d031d039b1e5473b030fa0f272f693b469d0ac0e"
+            (openpgp-fingerprint
+              "1EFB 0909 1F17 D28C CBF9  B13A 53D4 57B2 D636 EE82")))))
