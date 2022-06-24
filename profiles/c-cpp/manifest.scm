196c710d43785c70a371fbb3272c5b808c501619
Theo Tyburn <theo.tyburn@plasma.city>
2022-06-24 12:55:23 +0200

    (c-cpp): Add profile

---
+++ profiles/c-cpp/manifest.scm
@@ -0,0 +1,16 @@
+(specifications->manifest
+ (list
+  ;; build
+  "gcc-toolchain"
+  "clang"
+  "make"
+
+  ;; debugging
+  "gdb"
+  "valgrind"
+
+  ;; IDE
+  "ccls"
+  ;; "cppcheck"
+  ;; "global"
+  ))
